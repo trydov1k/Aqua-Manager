39f9724a414610382ef156e3862048dc55717cb7
Дмитрий <mdimon00@mail.ru>
2026-04-27 23:22:51 +0500

    Написал тесты для IncomeService и SaveLoadService

--- AquaManager/AquaManager.Tests/DomainTests/ServicesTests/IncomeServiceTests.cs
+++ AquaManager/AquaManager.Tests/DomainTests/ServicesTests/IncomeServiceTests.cs
@@ -1,10 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AquaManager.Domain.Constants;
+using AquaManager.Domain.Enums;
+using AquaManager.Domain.Models;
+using AquaManager.Domain.Services;
 
 namespace AquaManager.Tests.DomainTests.ServicesTests
 {
-    internal class IncomeServiceTests
+    [TestFixture]
+    public class IncomeServiceTests
     {
+        [Test]
+        public void CalculateTotalIncome_ShouldSumIncomeValueOfAllLiveFish()
+        {
+            var player = new Player(100, new List<Aquarium>(), 0);
+            var aquarium = new Aquarium("Test", 5);
+            var fish1 = CreateStandartFish();
+            var fish2 = CreateStandartFish();
+            var deadFish = CreateDeadFish();
+            aquarium.AddFish(fish1);
+            aquarium.AddFish(fish2);
+            aquarium.AddFish(deadFish);
+            player.Aquariums.Add(aquarium);
+
+            var service = new IncomeService(player);
+            
+            bool incomeRaised = false;
+            decimal raisedAmount = 0;
+            service.IncomeGenerated += (s, e) => { incomeRaised = true; raisedAmount = e.Amount; };
+            service.Start();
+            
+            var method = typeof(IncomeService).GetMethod("CalculateTotalIncome", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var total = (decimal)method.Invoke(service, null);
+            Assert.AreEqual(FishIncomeValue + FishIncomeValue, total);
+            service.Stop();
+            service.Dispose();
+        }
+
+        #region Вспомогательные методы
+        private static (decimal, double, string, decimal) GuppyConstants = GameConstants.FishByTypeDict[FishType.Guppy];
+        private static string FishName = GuppyConstants.Item3;
+        private static decimal FishPrice = GuppyConstants.Item1;
+        private static double FishHungerRate = GuppyConstants.Item2;
+        private static decimal FishIncomeValue = GuppyConstants.Item4;
+        public Fish CreateStandartFish() => new Fish(FishName, FishType.Guppy, FishHungerRate, FishPrice, FishIncomeValue);
+        private Fish CreateHungredFish()
+        {
+            var fish = CreateStandartFish();
+            while (fish.Hunger - FishHungerRate > 0)
+                fish.UpdateHunger();
+
+            Assert.AreEqual(FishHungerRate, fish.Hunger, 0.001);
+            return fish;
+        }
+        private Fish CreateDeadFish()
+        {
+            var fish = CreateHungredFish();
+            fish.UpdateHunger();
+
+            Assert.IsTrue(fish.IsDie);
+
+            return fish;
+        }
+        #endregion
     }
 }
--- AquaManager/AquaManager.Tests/DomainTests/ServicesTests/SaveLoadServiceTests.cs
+++ AquaManager/AquaManager.Tests/DomainTests/ServicesTests/SaveLoadServiceTests.cs
@@ -1,10 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AquaManager.Domain.Constants;
+using AquaManager.Domain.Enums;
+using AquaManager.Domain.Models;
+using AquaManager.Domain.Services;
 
-namespace AquaManager.Tests.DomainTests.ServicesTests
+namespace AquaManager.Tests.DomainTests.ServicesTests;
+
+[TestFixture]
+public class SaveLoadServiceTests
 {
-    internal class SaveLoadServiceTests
+    private string _tempFile;
+    private SaveLoadService _service;
+    private Player _originalPlayer;
+
+    [SetUp]
+    public void SetUp()
     {
+        _tempFile = Path.GetTempFileName();
+        _service = new SaveLoadService(_tempFile);
+        _originalPlayer = new Player(500, new List<Aquarium>(), 0);
+        var aqua = new Aquarium("TestAqua", 3);
+        var fish = CreateStandartFish();
+        aqua.AddFish(fish);
+        _originalPlayer.Aquariums.Add(aqua);
     }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (File.Exists(_tempFile)) File.Delete(_tempFile);
+    }
+
+    [Test]
+    public void SaveGame_ShouldCreateFile()
+    {
+        _service.SaveGame(_originalPlayer);
+        Assert.IsTrue(File.Exists(_tempFile));
+        Assert.IsTrue(new FileInfo(_tempFile).Length > 0);
+    }
+
+    [Test]
+    public void LoadGame_ShouldRestorePlayerCorrectly()
+    {
+        _service.SaveGame(_originalPlayer);
+        var loaded = _service.LoadGame();
+        Assert.AreEqual(_originalPlayer.Money, loaded.Money);
+        Assert.AreEqual(_originalPlayer.Aquariums.Count, loaded.Aquariums.Count);
+        Assert.AreEqual(_originalPlayer.Aquariums[0].Name, loaded.Aquariums[0].Name);
+        Assert.AreEqual(_originalPlayer.Aquariums[0].FishList[0].Name, loaded.Aquariums[0].FishList[0].Name);
+        Assert.AreEqual(_originalPlayer.Aquariums[0].FishList[0].Hunger, loaded.Aquariums[0].FishList[0].Hunger);
+    }
+
+    [Test]
+    public void LoadGame_WhenFileNotExists_ReturnsNullAndRaisesError()
+    {
+        if (File.Exists(_tempFile)) File.Delete(_tempFile);
+        bool errorRaised = false;
+        _service.ErrorOccurred += (s, msg) => errorRaised = true;
+        var loaded = _service.LoadGame();
+        Assert.IsNull(loaded);
+        Assert.IsTrue(errorRaised);
+    }
+
+    [Test]
+    public void SaveFileExists_ReturnsCorrectState()
+    {
+        if (File.Exists(_tempFile)) File.Delete(_tempFile);
+        Assert.IsFalse(_service.SaveFileExists());
+        _service.SaveGame(_originalPlayer);
+        Assert.IsTrue(_service.SaveFileExists());
+    }
+
+    [Test]
+    public void DeleteSaveFile_RemovesFile()
+    {
+        _service.SaveGame(_originalPlayer);
+        Assert.IsTrue(File.Exists(_tempFile));
+        _service.DeleteSaveFile();
+        Assert.IsFalse(File.Exists(_tempFile));
+    }
+
+    #region Вспомогательные методы
+    private static (decimal, double, string, decimal) GuppyConstants = GameConstants.FishByTypeDict[FishType.Guppy];
+    private static string FishName = GuppyConstants.Item3;
+    private static decimal FishPrice = GuppyConstants.Item1;
+    private static double FishHungerRate = GuppyConstants.Item2;
+    private static decimal FishIncomeValue = GuppyConstants.Item4;
+    public Fish CreateStandartFish() => new Fish(FishName, FishType.Guppy, FishHungerRate, FishPrice, FishIncomeValue);
+    
+    #endregion
 }
